6476313e6c4d8e0b6c552fa8e5c6ba0bac6eb8e9
jiuliogusmao <jiuliogusmao@gmail.com>
2022-10-25 17:35:49 -0300

    projeto criado

---
+++ src/chess/system/ChessSystem.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package chess.system;
+
+/**
+ *
+ * @author jiulio
+ */
+public class ChessSystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+    
+}
