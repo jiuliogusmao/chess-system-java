35fc12c87c21ac02e3fcb1a96be598cc9e4c91b7
jiuliogusmao <jiuliogusmao@gmail.com>
2022-10-26 00:43:44 -0300

    Chesslayer and print the board

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,15 +2,15 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package chess.system;
+package application;
 
-import boardgame.Board;
+import chess.ChessMatch;
 
 /**
  *
  * @author jiulio
  */
-public class ChessSystem {
+public class Program {
 
     /**
      * @param args the command line arguments
@@ -18,8 +18,9 @@ public class ChessSystem {
     public static void main(String[] args) {
         
         
-        Board board = new Board (8, 8);
-        System.out.println("Board");
+        ChessMatch chessMatch = new ChessMatch();
+        UI.printBoard(chessMatch.getPieces());
+        
     }
     
 }
