3439a757766fd502a7af9246f196245a84516185
jiuliogusmao <jiuliogusmao@gmail.com>
2022-10-26 22:12:41 -0300

    BoardException and defensive programming

---
+++ src/boardgame/BoardException.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package boardgame;
+
+/**
+ *
+ * @author jiulio
+ */
+public class BoardException extends RuntimeException{
+    private static final long serialVerisonUID = 1L;
+    
+    public BoardException(String msg){
+        super(msg);
+    }
+}
